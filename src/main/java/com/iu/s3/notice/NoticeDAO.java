9e4ccd0342aedc3e6f7eb067127a70638bb302f4
sunghyun0216 <sunghyun940216@gmail.com>
2021-03-26 17:18:01 +0900

    요맘때3

--- src/main/java/com/iu/s3/notice/NoticeDAO.java
+++ src/main/java/com/iu/s3/notice/NoticeDAO.java
@@ -1,5 +1,27 @@
 package com.iu.s3.notice;
 
-public class NoticeDAO {
+import java.util.List;
 
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.iu.s3.bankbook.BankBookDTO;
+
+@Repository
+public class NoticeDAO {
+	
+	@Autowired
+	private SqlSession sqlSession;
+	private final String NAMESPACE="com.iu.s3.notice.NoticeDAO";
+	
+	public List<NoticeDTO> getList()throws Exception{
+		return sqlSession.selectList(NAMESPACE+"getList");
+	}
+	
+	public BankBookDTO getSelect(BankBookDTO bankBookDTO)throws Exception{
+		bankBookDTO = sqlSession.selectOne(NAMESPACE+"getSelect", bankBookDTO);
+		return bankBookDTO;
+	}
+	
 }
--- src/test/java/com/iu/s3/notice/NoticeDAOTest.java
+++ src/test/java/com/iu/s3/notice/NoticeDAOTest.java
@@ -2,13 +2,35 @@ package com.iu.s3.notice;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-public class NoticeDAOTest {
+import com.iu.s3.MyAbstractTest;
+import com.iu.s3.bankbook.BankBookDTO;
 
+public class NoticeDAOTest extends MyAbstractTest {
+
+	@Autowired
+	private NoticeDAO noticeDAO;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void getListTest()throws Exception{
+		List<NoticeDTO> ar = noticeDAO.getList();
+		assertNotEquals(0, ar.size());
+	}
+	
+	@Test
+	public void getSelectTest()throws Exception{
+		NoticeDTO noticeDTO = new NoticeDTO();
+		noticeDTO.setNum(1);
+		noticeDTO.setTitle("home");
+		noticeDTO.setWriter("iu");
+		noticeDTO.setContents("과제는 무서워");
+		noticeDTO.setHit(1);
+		noticeDTO.setRegdate(null);
+		
 	}
 
 }
