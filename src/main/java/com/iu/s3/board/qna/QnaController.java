2ac7bb02e9e92c1a840743ad3911f6c4f4674a23
sunghyun0216 <sunghyun940216@gmail.com>
2021-04-05 00:02:49 +0900

    아 몰랑

--- src/main/java/com/iu/s3/board/qna/QnaController.java
+++ src/main/java/com/iu/s3/board/qna/QnaController.java
@@ -15,10 +15,36 @@ import com.iu.s3.util.Pager;
 @Controller
 @RequestMapping("/qna/**")
 public class QnaController {
-	
+
 	@Autowired
 	private QnaService qnaService;
-	
+
+	@GetMapping("qnaDelete")
+	public String setDelete(BoardDTO boardDTO)throws Exception{
+		int result = qnaService.setDelete(boardDTO);
+
+		return "redirect:../";
+	}
+
+	@GetMapping("qnaUpdate")
+	public ModelAndView setUpdate(BoardDTO boardDTO, ModelAndView mv)throws Exception{
+		boardDTO = qnaService.getSelect(boardDTO);
+		mv.addObject("dto", boardDTO);
+		mv.addObject("board", "qna");
+		mv.setViewName("board/boardUpdate");
+		return mv;
+	}
+
+	@GetMapping("qnaSelect")
+	public ModelAndView getSelect(BoardDTO boardDTO)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		boardDTO = qnaService.getSelect(boardDTO);
+		mv.addObject("dto", "qna");
+		mv.addObject("board", "qna");
+		mv.setViewName("board/boardSelect");
+		return mv;
+	}
+
 	@GetMapping("qnaList")
 	public ModelAndView getList(Pager pager)throws Exception{
 		ModelAndView mv = new ModelAndView();
@@ -26,16 +52,16 @@ public class QnaController {
 		mv.addObject("list", ar);
 		mv.addObject("board", "qna");
 		mv.setViewName("board/boardList");
-		
+
 		return mv;	
 	}
-	
+
 	@GetMapping("qnaInsert")
 	public ModelAndView setInsert()throws Exception{
 		ModelAndView mv = new ModelAndView();
 		mv.setViewName("board/boardInsert");
 		mv.addObject("board", "qna");
-		
+
 		return mv;
 	}
 
