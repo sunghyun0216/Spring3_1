2f8072597b18d76bd9b3febb12939e3b63b963dc
sunghyun0216 <sunghyun940216@gmail.com>
2021-04-09 10:14:51 +0900

    4/9

--- src/main/java/com/iu/s3/HomeController.java
+++ src/main/java/com/iu/s3/HomeController.java
@@ -8,6 +8,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,6 +19,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	@GetMapping("/test")
+	public void test(int num, Model model) {
+		System.out.println(num);
+		model.addAttribute("str", "Ajax"); //str이라는 속성에다가 ajax란 문자열을 보냄
+	}
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
